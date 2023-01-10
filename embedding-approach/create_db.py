838e34abe26ec1b91ae27c599e4b29c3895bac29
lasklu <lukas.laskowski@student.hpi.de>
2023-01-10 20:06:40 +0000

    modifying create database script so that it creates npy files and applying image transform and further things

--- embedding-approach/create_db.py
+++ embedding-approach/create_db.py
@@ -1,30 +1,38 @@
 import os
 import csv
-import cv2
+from cv2 import imread
 from model.triplet import TripletLoss
 import torch
-
+import json
+import pandas as pd
+import numpy as np
+from utils.image import transform_image
 def main():
-    image_folder = ""
-    db_file = ""
-    pretrained_model_path = ""
-    
-    if not os.path.exists(db_file):
-        with open(db_file, 'w') as f:
-            writer = csv.writer(f)
-            writer.writerow(["label", "embedding"])
+    image_folder = "/data/cropped"
+    db_file = "/gorilla-reidentification/embedding-approach/database"
+    pretrained_model_path = "/data/models/epoch=2-val_loss=0.69-val_acc=1.59.ckpt"
+    config_path = "/gorilla-reidentification/embedding-approach/configs/config.json"
+    with open(config_path) as config_buffer:    
+        config = json.loads(config_buffer.read())
 
     model = TripletLoss.load_from_checkpoint(pretrained_model_path)
-
-    with open(db_file, 'a') as db:
-        db_writer = csv.writer(db)
-        for img_file in os.listdir(image_folder):
+    labels = []
+    embeddings = []
+    for folder in os.listdir(image_folder):
+        if folder == 'script.sh':
+            continue
+        for img_file in os.listdir(os.path.join(image_folder, folder)):
             label, ext = os.path.splitext(img_file)
             if ext not in [".png", ".jpg", ".jpeg"]:
                 continue
             with torch.no_grad():
-                img_path = os.path.join(image_folder, img_file)
-                db_writer.writerow([label, model(cv2.imread(img_path))])
+                img_path = os.path.join(image_folder,folder, img_file)
+                img = transform_image(imread(img_path), (config['model']['input_width'], config['model']['input_height']))
+                labels.append(label[:4])
+                embeddings.append(model(img).numpy()[0])
+    np.save(os.path.join(config["predict"]["db_path"], "labels.npy"), np.array(labels))
+    np.save(os.path.join(config["predict"]["db_path"], "embeddings.npy"), np.array(embeddings))
+    
 
 if __name__ == '__main__':
     main()
