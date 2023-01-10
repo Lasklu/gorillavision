f37d16480797efb4d21f0620d8c94da3b802ca5d
lasklu <lukas.laskowski@student.hpi.de>
2023-01-10 20:05:55 +0000

    finishing first version of predict function

--- embedding-approach/predict.py
+++ embedding-approach/predict.py
@@ -1,6 +1,6 @@
 from model.triplet import TripletLoss
 from utils.dataset_utils import load_data
-from sklearn.neighbors import NearestNeighbors
+from sklearn import neighbors
 import torch
 from torchvision.transforms import Compose, Resize, ToPILImage, ToTensor
 from cv2 import imread
@@ -8,6 +8,7 @@ import numpy as np
 import argparse
 import os
 import json
+from utils.image import transform_image
 import pandas as pd
 
 argparser = argparse.ArgumentParser()
@@ -25,38 +26,31 @@ def main():
     # Load Model
     model = TripletLoss.load_from_checkpoint(f"{config['predict']['model_path']}/{model_name}")
     img = transform_image(imread(config["predict"]["img_path"]), (config['model']['input_width'], config['model']['input_height']))
-    print("shape", np.shape(img))
     #img=torch.from_numpy(img).float()
     with torch.no_grad():
         predicted_embedding = model(img)
 
     # Load embedding_db file
-    db = pd.read_csv(config["predict"]["db_path"])
-    embeddings = db["label"].tolist()
-    labels = db["embedding"].tolist()
-
+    #db = pd.read_csv(config["predict"]["db_path"])
+    labels = np.load(os.path.join(config["predict"]["db_path"],'labels.npy'))
+    embeddings = np.load(os.path.join(config["predict"]["db_path"],'embeddings.npy'))
+    
     # fit kNN classifier model on db
-    nn_classifier = NearestNeighbors(metric='euclidean')
+    nn_classifier = neighbors.KNeighborsClassifier()
     nn_classifier.fit(embeddings, labels)
-
+    print(nn_classifier.predict(predicted_embedding.numpy()))
     # predict k nearest neighbours
-    distances, indices  = nn_classifier.kneighbors(predicted_embedding, n_neighbors=10)
-    neighbour_labels = [labels[i] for i in indices]
+    
+    #distances, indices  = nn_classifier.kneighbors(predicted_embedding, n_neighbors=10)
+    #neighbour_labels = [labels[i] for i in indices]
 
-    print(neighbour_labels)
+    #print(neighbour_labels)
     
     # Based on top-10 predictions and the according distance, apply a treshhold to check whether we are dealing 
     # with a new individual. If so, add it to db with new label.
     # if not return a majority vote of knn or the first as the prediction for this input
 
-def transform_image(img, img_size):
-    transformations = Compose([
-            ToPILImage(),
-            Resize(img_size),
-            ToTensor(),
-        ])
-    img = transformations(img)
-    return torch.unsqueeze(s, dim=0) #remove this when having multiple images
+
 
 if __name__ == "__main__":
     main()
