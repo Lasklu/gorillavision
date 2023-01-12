6026ef2570a4b32dc10e031c085b6da910e60e4f
lasklu <lukas.laskowski@student.hpi.de>
2023-01-12 09:30:52 +0000

    modifying predict

--- embedding-approach/predict.py
+++ embedding-approach/predict.py
@@ -7,8 +7,12 @@ import numpy as np
 from sklearn import neighbors
 import torch
 from torchvision.transforms import Compose, Resize, ToPILImage, ToTensor
-from model.triplet import TripletLoss
-from utils.dataset_utils import load_data
+from cv2 import imread
+import numpy as np
+from cv2 import imread
+import argparse
+import os
+import json
 from utils.image import transform_image
 
 argparser = argparse.ArgumentParser()
@@ -24,13 +28,48 @@ def main():
     with open(config_path) as config_buffer:    
         config = json.loads(config_buffer.read())
 
+    # Load Model
+    model = TripletLoss.load_from_checkpoint(f"{config['predict']['model_path']}/{model_name}")
+    #img = transform_image(imread(config["predict"]["img_path"]), (config['model']['input_width'], config['model']['input_height']))
+    #img=torch.from_numpy(img).float()
+    #with torch.no_grad():
+    #    predicted_embedding = model(img)
 
     # Load Model and DB and fit kNN-classifier on DB
     model = TripletLoss.load_from_checkpoint(f"{config['predict']['model_path']}")
     labels = np.load(os.path.join(config["predict"]["db_path"],'labels.npy'))
     embeddings = np.load(os.path.join(config["predict"]["db_path"],'embeddings.npy'))
-    nn_classifier = neighbors.KNeighborsClassifier(n_neighbors=config["predict"]["n_neighbors"])
+    
+    # fit kNN classifier model on db
+    nn_classifier = neighbors.KNeighborsClassifier()
+    print("training")
     nn_classifier.fit(embeddings, labels)
+    test_labels = []
+    test_embeddings = []
+    image_folder = "/data/data/gorillas_dante/face_cropped/train_test/test"
+    for folder in os.listdir(image_folder):
+        if folder == 'script.sh':
+            continue
+        print(folder)
+        for img_file in os.listdir(os.path.join(image_folder, folder)):
+            label, ext = os.path.splitext(img_file)
+            if ext not in [".png", ".jpg", ".jpeg"]:
+                continue
+            with torch.no_grad():
+                img_path = os.path.join(image_folder,folder, img_file)
+                img = transform_image(imread(img_path), (config['model']['input_width'], config['model']['input_height']))
+                test_labels.append(folder)
+                print("transformed, applying model")
+                test_embeddings.append(model(img).numpy()[0])
+    test_labels = np.array(test_labels)
+    test_embeddings = np.array(test_embeddings)
+    print("scoring")
+    score = nn_classifier.score(test_embeddings, test_labels)
+    print("scored", score)
+    # predict k nearest neighbours
+    
+    #distances, indices  = nn_classifier.kneighbors(predicted_embedding, n_neighbors=10)
+    #neighbour_labels = [labels[i] for i in indices]
 
     total_predictions = []
 
