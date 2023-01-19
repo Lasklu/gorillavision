96ee4c3040cba198eb314c3b52c96487703198b7
lasklu <lukas.laskowski@student.hpi.de>
2023-01-19 09:23:46 +0000

    continue owrking on metrics

--- embedding-approach/utils/metrics.py
+++ embedding-approach/utils/metrics.py
@@ -1,14 +1,23 @@
+from sklearn.metrics import f1_score, confusion_matrix, precision_score, recall_score, accuracy_score
+
 def is_in_top_x(neighbour_predictions, real_label, x):
     return real_label in neighbour_predictions[:x]
 
-def top_x_accuracy(predictions, x):
+def top_k_accuracy(predictions, x):
     pass
 
-def mean_average_precision(predictions):
-    pass
-
-def compute_prediction_metrics(predictions, x):
-    return {
-        f"top-{str(x)}-accuracy": top_x_accuracy(predictions, x),
-        "mAP": mean_average_precision(predctions)
+def compute_prediction_metrics(y_true, y_pred):
+    #f"top-{str(x)}-accuracy": top_x_accuracy(predictions, x),
+    #    "mAP": mean_average_precision(predctions),
+    print("Recall", recall_score(y_true, y_pred,average='micro'))
+    print("Precision", recall_score(y_true, y_pred,average='micro'))
+    print("F1Score", f1_score(y_true, y_pred, average='micro'))
+    print("Accuracy", accuracy_score(y_true, y_pred))
+    print("Confusion,matrix", confusion_matrix(y_true, y_pred))
+    return {   
+        "recall": recall_score(y_true, y_pred,average='micro'),
+        "precision": precision_score(y_true, y_pred,average='micro'),
+        "f1Score": f1_score(y_true, y_pred, average='micro'),
+        "accuracy": accuracy_score(y_true, y_pred),
+        "consusion_matrix": confusion_matrix(y_true, y_pred)
     }
