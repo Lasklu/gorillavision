43ddac0bcf21d8bb99bb75045aaf08e7372a1477
rohansaw <rohan.sawahn@student.hpi.de>
2023-02-01 19:27:18 +0000

    using new named paths

--- scripts/create_ds_2.py
+++ scripts/create_ds_2.py
@@ -2,12 +2,11 @@ import os
 import shutil
 import random
 
-#This scripts takes defined datasets and splits them into train,test and eval sets.
+#This scripts takes defined datasets and splits them into train,test and eeval sets.
 base_path = "/scratch1/all_gorillas"
 output_path = os.path.join("/scratch1/gorilla_experiment_splits/train_test_non_distinct")
 bristol_dataset_faces = os.path.join(base_path, 'bristol', 'face_images')
 cxl_dataset_faces = os.path.join(base_path, 'cxl', 'face_images_grouped')
-
 
 datasets = [
 	{
@@ -26,7 +25,7 @@ datasets = [
 		'datasets': [
 			{'dataset_path': bristol_dataset_faces,
 			'min_elements_per_id': None,
-			'exact_elements_per_id': 15,
+			'exact_elements_per_id': 10,
 			'train_test_split': {
 				'distinct': False,
 				'percentage': 0.7
@@ -116,24 +115,33 @@ def copy_to(files, id, folder, ds_path, ds_identifier):
 		target_path = os.path.join(output_path, ds_identifier, folder, id, file_name)
 		shutil.copy(file_path, target_path)
 
-def split_train_test_non_distinct(files, percentage, min_el, ds_path, ds_identifier, id):
+def split_train_test_non_distinct(files, percentage, min_el, ds_path, ds_identifier, id, exact_elements_per_id):
 	# if not enough images per for this individual, copy to test
 	if min_el != None and not len(files[:round(len(files) * percentage)]) >= min_el:
-		copy_to(files, id, "test", ds_path, ds_identifier)
 		return
-	
-	copy_to(files[:round(len(files) * percentage)], id, "train", ds_path, ds_identifier)
-	copy_to(files[round(len(files) * percentage):], id, "test", ds_path, ds_identifier)
+
+	if exact_elements_per_id != None:
+		copy_to(files[:exact_elements_per_id], id, "train", ds_path, ds_identifier)
+		copy_to(files[:exact_elements_per_id], id, "database_set", ds_path, ds_identifier)
+		if not os.path.exists(os.path.join(output_path, ds_identifier, 'eval', id)):
+			os.mkdir(os.path.join(output_path, ds_identifier, 'eval', id))
+		copy_to(files[exact_elements_per_id:], id, "eval", ds_path, ds_identifier)
+	else:
+		copy_to(files[:round(len(files) * percentage)], id, "train", ds_path, ds_identifier)
+		copy_to(files[:round(len(files) * percentage)], id, "database_set", ds_path, ds_identifier)
+		if not os.path.exists(os.path.join(output_path, ds_identifier, 'eval', id)):
+			os.mkdir(os.path.join(output_path, ds_identifier, 'eval', id))
+		copy_to(files[round(len(files) * percentage):], id, "eval", ds_path, ds_identifier)
 
 def split_train_test_distinct(files, percentage, min_el, ds_path, ds_identifier, id, train_classes):
 	if min_el != None and not len(files) >= min_el:
-		copy_to(files, id, "test", ds_path, ds_identifier)
+		copy_to(files, id, "database_set", ds_path, ds_identifier)
 		return
 	
 	if id in train_classes:
 		copy_to(files, id, "train", ds_path, ds_identifier)
 	else:
-		copy_to(files, id, "test", ds_path, ds_identifier)
+		copy_to(files, id, "database_set", ds_path, ds_identifier)
 
 def get_distinct_classes_split(folders_path, percentage):
 	all_classes = os.listdir(folders_path)
@@ -157,42 +165,44 @@ def main():
 		identifier = f"{dataset['name']}"
 		make_folder(identifier, output_path)
 		make_folder(f"{identifier}/train", output_path)
-		make_folder(f"{identifier}/test", output_path)
-		make_folder(f"{identifier}/val", output_path)
+		make_folder(f"{identifier}/database_set", output_path)
+		make_folder(f"{identifier}/eval", output_path)
 		for sub_dataset in dataset['datasets']:
 			# only used if distinct train_test split mode
 			train_classes, test_classes = get_distinct_classes_split(sub_dataset['dataset_path'], sub_dataset['train_test_split']['percentage'])
-
-			if "exact_elements_per_id" in sub_dataset:
+			
+			exact_elements_per_id = sub_dataset["exact_elements_per_id"] if "exact_elements_per_id" in sub_dataset else None
+			"""if exact_elements_per_id:
 				temp_path = os.path.join(output_path, "temp")
 				if not os.path.exists(temp_path):
 					os.mkdir(temp_path)
 					make_reduced_ds(sub_dataset, temp_path)
-					sub_dataset['dataset_path'] = temp_path
+					sub_dataset['dataset_path'] = temp_path"""
 
 			for id in os.listdir(sub_dataset['dataset_path']):
 				files = os.listdir(f"{sub_dataset['dataset_path']}/{id}")
 				random.shuffle(files)
 				if not sub_dataset["train_test_split"]["distinct"]:
 					split_train_test_non_distinct(files, sub_dataset['train_test_split']['percentage'],
-												  sub_dataset["min_elements_per_id"], sub_dataset['dataset_path'], identifier, id)
+												  sub_dataset["min_elements_per_id"], sub_dataset['dataset_path'], identifier, id, exact_elements_per_id)
 				else:
 					split_train_test_distinct(files, sub_dataset['train_test_split']['percentage'],
 											  sub_dataset["min_elements_per_id"], sub_dataset['dataset_path'], identifier, id, train_classes)
 			
-			# create validation set with part of the data from test
-			for id in os.listdir(os.path.join(f"{output_path}/{identifier}", "test")):
-				files = os.listdir(os.path.join(f"{output_path}/{identifier}", "test", id))
-				random.shuffle(files)
-				for file_name in files[:round(len(files) * 0.25)]:
-					if not os.path.exists(os.path.join(output_path, identifier, 'val', id)):
-						os.mkdir(os.path.join(output_path, identifier, 'val', id))
-					file_path = os.path.join(f"{output_path}/{identifier}", "test", id, file_name)
-					target_path = os.path.join(output_path, identifier, 'val', id, file_name)
-					shutil.move(file_path, target_path)
+			# create evalidation set with part of the data from test
+			if not exact_elements_per_id and sub_dataset["train_test_split"]["distinct"] == True:
+				for id in os.listdir(os.path.join(f"{output_path}/{identifier}", "database_set")):
+					files = os.listdir(os.path.join(f"{output_path}/{identifier}", "database_set", id))
+					random.shuffle(files)
+					for file_name in files[:round(len(files) * 0.3)]:
+						if not os.path.exists(os.path.join(output_path, identifier, 'eval', id)):
+							os.mkdir(os.path.join(output_path, identifier, 'eval', id))
+						file_path = os.path.join(f"{output_path}/{identifier}", "database_set", id, file_name)
+						target_path = os.path.join(output_path, identifier, 'eval', id, file_name)
+						shutil.move(file_path, target_path)
 			
-			if "exact_elements_per_id" in sub_dataset:
-				shutil.rmtree(os.path.join(output_path, "temp"))
+			"""if "exact_elements_per_id" in sub_dataset:
+				shutil.rmtree(os.path.join(output_path, "temp"))"""
 
    
 def make_folder(folder_name, folder_path):
