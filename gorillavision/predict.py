6e410a15339b078dfd8ef600903d327f0fae3d1c
rohansaw <rohan.sawahn@student.hpi.de>
2023-03-02 10:36:28 +0000

    added prediction code

--- gorillavision/predict.py
+++ gorillavision/predict.py
@@ -16,19 +16,20 @@ from detection_helpers import *
 from tracking_helpers import *
 from prediction_utils import *
 
-class GorillaVision():
-    def __init___(self, file_paths, face_detection_model_path, body_detection_model_path, stageII_model_path, db_folder, out_folder):
+class GorillaVision:
+
+    def __init__(self, file_paths, face_detection_model_path, body_detection_model_path, stageII_model_path, db_folder, out_folder):
         self.file_paths = file_paths
         self.out_folder = out_folder
 
         self.detector_face = Detector()
         self.detector_body = Detector()
-        detector_face.load_model(face_detection_model_path)
-        detector_body.load_model(body_detection_model_path)
+        self.detector_face.load_model(face_detection_model_path)
+        self.detector_body.load_model(body_detection_model_path)
 
         if contains_video(self.file_paths):
-            self.tracker_face = YOLOv7_DeepSORT(reID_model_path="./deep_sort/model_weights/mars-small128.pb", detector=detector_face)
-            self.tracker_body = YOLOv7_DeepSORT(reID_model_path="./deep_sort/model_weights/mars-small128.pb", detector=detector_body)
+            self.tracker_face = YOLOv7_DeepSORT(reID_model_path="./deep_sort/model_weights/mars-small128.pb", detector=self.detector_face)
+            self.tracker_body = YOLOv7_DeepSORT(reID_model_path="./deep_sort/model_weights/mars-small128.pb", detector=self.detector_body)
             self.video_cap = None
 
         self.init_identity_model(stageII_model_path, db_folder)
@@ -42,59 +43,83 @@ class GorillaVision():
         self.knn_classifier.fit(db_embeddings, db_labels)
 
     def predict_all(self):
-        for file_path in self.file_path:
-            predict_vilde(file_path)
-            """if is_video(file_path):
-                predict_video(file_path)
+        for file_path in self.file_paths:
+            if is_video(file_path):
+                self.predict_video(file_path)
             elif is_img(file_path):
-                predict_img(file_path)
+                self.predict_img(file_path)
             else:
-                raise Exception("Trying to predict unknown file type")"""
+                raise Exception("Trying to predict unknown file type")
             
     def predict_identity(self, img):
         img = transform_image(img, self.identity_model_imgsz, "crop")
         with torch.no_grad():
             predicted_embedding = self.identity_model(img).numpy()
             predicted_id = self.knn_classifier.predict(predicted_embedding)
-            return predicted_id
+            return predicted_id[0]
 
     def predict_img(self, img_path):
-        pass
+        full_img = cv2.imread(img_path)
+        detector_res = self.detector_face.detect(full_img, plot_bb = False)
+        if len(detector_res) != 0:
+            bboxes = detector_res[:,:4]
+            scores = detector_res[:,4]
+            classes = detector_res[:,-1]
+            num_objects = bboxes.shape[0]
+        else:
+            bboxes = []
+            scores = []
+            classes = []
+            num_objects = 0
+        
+        cropped_imgs = [self.crop_to_bbox(full_img, [int(v) for v in bbox]) for bbox in bboxes]
+        results = [self.predict_identity(img) for img in cropped_imgs]
 
-    def crop_to_bbox(self, frame_idx, bbox):
-        video_cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
-        _, unprocessed_img = video_cap.read()
-        bbox = [int(v) for v in bbox]
-        cropped_img = unprocessed_img[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+        for bbox, identity in zip(bboxes, results):
+            draw_label(full_img, bbox, identity, (255,0,0))
+        out_path = os.path.join(self.out_folder, os.path.basename(img_path))
+        cv2.imwrite(out_path, full_img)
+
+        
+    def get_img_at_frame(self, frame_idx):
+        self.video_cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
+        ret, img = self.video_cap.read()
+        return img
+
+    def crop_to_bbox(self, img, bbox):
+        cropped_img = img[bbox[1]:bbox[3], bbox[0]:bbox[2]]
         return cropped_img
 
     def predict_video(self, video_path):
         self.video_cap = cv2.VideoCapture(video_path)
-        frame_results_body = tracker_body.track_video(video_path, output=out_path, show_live = False, skip_frames = 0, count_objects = True, verbose=1)
-        frame_results_face = tracker_face.track_video(video_path, output=out_path, show_live = False, skip_frames = 0, count_objects = True, verbose=1)
+        frame_results_body = self.tracker_body.track_video(video_path, output=None, show_live = False, skip_frames = 0, count_objects = True, verbose=1)
+        frame_results_face = self.tracker_face.track_video(video_path, output=None, show_live = False, skip_frames = 0, count_objects = True, verbose=1)
         body_tracks = make_tracks(frame_results_body)
         face_tracks = make_tracks(frame_results_face)
 
         identities = {}
         for track_key in face_tracks.keys():
             face_track = face_tracks[track_key]
-            amount_to_select = min(len(tracks[track_id]), 5)
-            selection = np.random.choice(tracks[track_id], amount_to_select, replace=False)
-            imgs = [self.crop_to_bbox(res["frame_idx"], res["bbox"]) for res in selection]
+            amount_to_select = min(len(face_track), 5)
+            selection = np.random.choice(face_track, amount_to_select, replace=False)
+            imgs = [self.crop_to_bbox(self.get_img_at_frame(res["frame_idx"]), [int(v) for v in res["bbox"][0]]) for res in selection]
             predictions = [self.predict_identity(img) for img in imgs]
             majority_element = max(set(predictions), key=predictions.count)
             identities[track_key] = majority_element
 
         face_tracks = join_tracks(face_tracks, identities)
+        # ToDo: map faces to the corresponding body, to have a nicer vizualization over the body tracks
         # body_tracks_identities = map_body_to_faceID(body_tracks, face_tracks)
-        self.save_as_video(body_tracks, body_tracks.keys())
+        # self.save_as_video(body_tracks, body_tracks_identities, out_path)
+        out_path = os.path.join(self.out_folder, os.path.splitext(str(os.path.basename(video_path)))[0] + ".avi")
+        self.save_as_video(face_tracks, out_path)
         self.video_cap.release()
 
-    def save_as_video(self, tracks, track_identities):
-        video_cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        fps = video_cap.get(cv2.CAP_PROP_FPS)
-        w = int(video_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        h = int(video_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    def save_as_video(self, tracks, out_path):
+        self.video_cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        fps = self.video_cap.get(cv2.CAP_PROP_FPS)
+        w = int(self.video_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        h = int(self.video_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         codec = cv2.VideoWriter_fourcc(*"XVID")
         vid_writer = cv2.VideoWriter(out_path, codec, fps, (w, h))
 
@@ -102,7 +127,7 @@ class GorillaVision():
         total_boxes = 0
         colors = [(255,0,0), (0,255,0), (0,0,255), (255,255,0), (0,255,255), (255,0,255), (0,128,0), (128,0,128), (245,222,179)]
         while True:
-            ret, frame = video_cap.read()
+            ret, frame = self.video_cap.read()
             if not ret:
                 break
             for i, track_id in enumerate(tracks.keys()):
@@ -110,13 +135,8 @@ class GorillaVision():
                 if len(data) > 0:
                     data = data[0]
                     bbox = [int(v) for v in data["bbox"][0]]
-                    total_boxes += 1
-                    color = colors[i]
-                    identity = track_identities[track_id]
-                    cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 3)
-                    cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len("Gorilla")+len(str(identity)))*17, int(bbox[1])), color, -1)
-                    cv2.putText(frame, "Gorilla" + " : " + str(identity),(int(bbox[0]), int(bbox[1]-11)),0, 1.2, (255,255,255),2, lineType=cv2.LINE_AA)    
-            
+                    draw_label(frame, bbox, track_id, colors[i])
+                    
             res = np.asarray(frame)
             res = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
             vid_writer.write(res)
@@ -124,12 +144,12 @@ class GorillaVision():
 
 
 if __name__ == '__main__':
-    files = ["/data/data/video1.MP4"]
-    body_model_path = "/data/models/yolov7_body_model.pt"
-    face_model_path = "/data/models/yolov7_face_model.pt"
-    model2_path = "/data/models/identification_model.ckpt"
-    db_folder = "/data/models/db"
-    out_path = "/data/predictions"
+    files = ["/data/demo/data/HU22.png", "/data/demo/data/RC01.png", "/data/demo/data/video1.MP4"]
+    body_model_path = "/data/demo/models/yolov7_body_model.pt"
+    face_model_path = "/data/demo/models/yolov7_face_model.pt"
+    model2_path = "/data/demo/models/identification_model.ckpt"
+    db_folder = "/data/demo/models/db"
+    out_path = "/data/demo/predictions"
 
-    model = GorillaVision(files, body_model_path, face_model_path, db_folder, out_folder)
+    model = GorillaVision(files, face_model_path, body_model_path, model2_path, db_folder, out_path)
     model.predict_all()
--- gorillavision/prediction_utils.py
+++ gorillavision/prediction_utils.py
@@ -1,3 +1,9 @@
+import cv2
+import os
+
+VIDEO_FORMATS = [".mp4", ".avi",]
+IMAGE_FORMATS = [".png", ".jpg", ".jpeg"]
+
 def make_tracks(frame_results):
     tracks = {}
 
@@ -16,14 +22,29 @@ def make_tracks(frame_results):
 
     return tracks
 
-def join_tracks(tracks, identitites):
+def join_tracks(tracks, identities):
     new_tracks = {}
-    for identity in identitites.keys():
+    for track_key in identities.keys():
+        identity = identities[track_key]
         if identity not in new_tracks:
-            new_tracks[identity] = tracks[identitites[identity]]
+            new_tracks[identity] = tracks[track_key]
         else:
-            new_tracks[identity] += tracks[identitites[identity]]
+            new_tracks[identity] += tracks[track_key]
     
     return new_tracks
+
+def is_img(file_path):
+    return os.path.splitext(file_path)[1].lower() in IMAGE_FORMATS
+
+def is_video(file_path):
+    return os.path.splitext(file_path)[1].lower() in VIDEO_FORMATS
+
+def contains_video(file_paths):
+    return any(is_video(file_path) for file_path in file_paths)
+
+def draw_label(img, bbox, identity, color):
+    cv2.rectangle(img, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 3)
+    cv2.rectangle(img, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len("Gorilla")+len(str(identity)))*17, int(bbox[1])), color, -1)
+    cv2.putText(img, "Gorilla" + " : " + str(identity),(int(bbox[0]), int(bbox[1]-11)),0, 1.2, (255,255,255),2, lineType=cv2.LINE_AA)    
 
     
